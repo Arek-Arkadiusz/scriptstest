e4043b18d4997da5e86c013456820a466de6a416
Arek-Arkadiusz <45485158+Arek-Arkadiusz@users.noreply.github.com>
2020-07-06 13:24:42 +0200

    Update WinRM.ps1

--- WinRM.ps1
+++ WinRM.ps1
@@ -1,2 +1,14 @@
 # Enable port 5986 on Azure VM (Run Powershell As admin):
 New-NetFirewallRule -Displayname "WinRm-HTTPS Allow PS Remoting" -direction inbound -LocalPort 5986 -Protocol tcp -Action allow
+
+$selector_set = @{
+    Address = (get-childitem -path Certificate::CurrentUser\My | Where-Object { $_.Subject -like "*$($env:computername)*" }).DnsNameList.Unicode
+    Transport = "HTTPS"
+}
+
+$value_set = @{
+    CertificateThumbprint = (get-childitem -path Certificate::CurrentUser\My | Where-Object { $_.Subject -like "*$($env:computername)*" }).Thumbprint
+}
+
+New-WSManInstance -ResourceURI "winrm/config/Listener" -SelectorSet $selector_set -ValueSet $value_set
+
