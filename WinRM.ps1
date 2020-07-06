c60b942f493552f4b5da5e05b547882606f39bf9
Arek-Arkadiusz <45485158+Arek-Arkadiusz@users.noreply.github.com>
2020-07-06 13:49:52 +0200

    Update WinRM.ps1

--- WinRM.ps1
+++ WinRM.ps1
@@ -2,13 +2,18 @@
 New-NetFirewallRule -Displayname "WinRm-HTTPS Allow PS Remoting" -direction inbound -LocalPort 5986 -Protocol tcp -Action allow
 
 $selector_set = @{
-    Address = (get-childitem -path Certificate::CurrentUser\My | Where-Object { $_.Subject -like "*$($env:computername)*" }).DnsNameList.Unicode
+    Address = '*'
     Transport = "HTTPS"
 }
 
 $value_set = @{
-    CertificateThumbprint = (get-childitem -path Certificate::CurrentUser\My | Where-Object { $_.Subject -like "*$($env:computername)*" }).Thumbprint
+    Hostname = (get-childitem -path Certificate::LocalMachine\My | Where-Object { $_.Subject -like "*$($env:computername)*" }).DnsNameList.Unicode
+    CertificateThumbprint = (get-childitem -path Certificate::LocalMachine\My | Where-Object { $_.Subject -like "*$($env:computername)*" }).Thumbprint
 }
 
+# winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname="";CertificateThumbprint=""}
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.wsman.management/new-wsmaninstance?view=powershell-5.1
 New-WSManInstance -ResourceURI "winrm/config/Listener" -SelectorSet $selector_set -ValueSet $value_set
 
+# Validate listener
+# WinRM e winrm/config/listener
